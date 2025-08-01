73953753108d8709b0678cf3803366a749f72aeb
wvvwcom <wvvwcom@qq.com>
2025-08-01 17:37:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,10 +17,10 @@
 # git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # 设置主路由静态IP
-# sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # 设置密码为空
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
 # 安装新主题 luci-theme-bootstrap-mod
 # git clone https://github.com/leshanydy2022/luci-theme-bootstrap-mod.git package/lean/luci-theme-bootstrap-mod
@@ -28,13 +28,13 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-defaul
 
 # 安装luci-app-smartdns和luci-app-adguardhome
 # rm -rf feeds/luci/applications/luci-app-smartdns
-rm -rf feeds/luci/applications/luci-app-adguardhome
+# rm -rf feeds/luci/applications/luci-app-adguardhome
 # git clone https://github.com/leshanydy2022/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
-git clone https://github.com/leshanydy2022/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+# git clone https://github.com/leshanydy2022/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 
 # 为adguardhome插件更换最新的版本
-rm -rf feeds/packages/net/adguardhome
-git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/adguardhome
+# rm -rf feeds/packages/net/adguardhome
+# git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/adguardhome
 
 # 安装uugamebooster
 # rm -rf feeds/luci/applications/luci-app-uugamebooster
@@ -76,7 +76,7 @@ git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/ad
 # sed -i 's/ImmortalWrt/ImmortalWrt-YDY/g' package/base-files/files/bin/config_generate
 
 # Modify filename, add date prefix
-#sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +"%Y%m%d")-/1' include/image.mk
+# sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +"%Y%m%d")-/1' include/image.mk
 
 # Modify ppp-down, add sleep 3
 # sed -i '$a\\\nsleep 3' package/network/services/ppp/files/lib/netifd/ppp-down
